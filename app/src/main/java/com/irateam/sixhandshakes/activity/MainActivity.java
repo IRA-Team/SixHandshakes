ed893a6374d2f04c0630a1c65f5c85fbe57e9e57
melkiy <monochkov@gmail.com>
2016-04-04 09:13:40 +0300

    Added login with vk

--- app/src/main/java/com/irateam/sixhandshakes/activity/MainActivity.java
+++ app/src/main/java/com/irateam/sixhandshakes/activity/MainActivity.java
@@ -1,9 +1,16 @@
 package com.irateam.sixhandshakes.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.irateam.sixhandshakes.R;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button_vk_login).setOnClickListener((v) -> {
+            VKSdk.login(MainActivity.this, VKScope.FRIENDS);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Log.e("KEKAN", "KEKAN");
+                /*startActivity(new Intent(MainActivity.this, MainActivity.class));
+                finish();*/
+            }
+
+            @Override
+            public void onError(VKError error) {
+            }
+        })) ;
     }
 }
